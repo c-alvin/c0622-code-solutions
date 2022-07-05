b849fc50c572c369e067f6bdf8e9ec703242e9f6
Alvin Cho <alvinxcho2@gmail.com>
2022-07-05 19:33:05 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,48 @@
+var width = 1;
+var height = 2;
+
+var area = width * height;
+console.log('The value of area:', area);
+console.log('The typeof area:', typeof area);
+
+var bill = 3;
+var payment = 6;
+
+var change = payment - bill;
+
+console.log('The Value of change:', change);
+console.log('The typeof change:', typeof change);
+
+var quizzes = 70;
+var midterm = 85;
+var final = 100;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('The value of:', grade);
+console.log('The typeof:', typeof grade);
+
+var firstName = 'Alvin';
+var lastName = 'Cho';
+
+var fullName = firstName + ' ' + lastName;
+console.log('The value of:', fullName);
+console.log('The typeof:', typeof fullName);
+
+var pH = 13;
+
+var isAcidic = pH < 7;
+console.log('The value of:', isAcidic);
+console.log('The typeof:', typeof isAcidic);
+
+var headcount = 15;
+
+var isSparta = (headcount === 300);
+console.log('The value of:', isSparta);
+console.log('The typeof:', typeof isSparta);
+
+var motto = fullName;
+
+motto = fullName + ' is the GOAT';
+
+console.log('The value of:', motto);
+console.log('The typeof:', typeof motto);
