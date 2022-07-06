7237d56a01c9f7b7e809b1daf5849176e69a3ac8
Alvin Cho <alvinxcho2@gmail.com>
2022-07-06 18:32:04 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,62 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    shipTo: 'JS Masher',
+    totalPrice: 34.00,
+    deliveryDate: 'August 8,2020',
+    products: [{
+      productName: 'JavaScript for Impatient Programmers',
+      author: 'Rauschmayer, Dr. Axel',
+      returnWindow: 'Sep 7, 2020',
+      price: 31.55
+    }]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderPlaced: 'July 19, 2020',
+    shipTo: 'JS Masher',
+    totalPrice: 44.53,
+    deliveryDate: 'July 20,2020',
+    products: [{
+      productName: 'The Timeless Way of Building',
+      author: 'Alexander, Christopher',
+      returnWindow: 'Aug 19, 2020',
+      price: 41.33
+    }]
+  },
+  {
+    orderNumber: '114-287557-9059409',
+    orderPlaced: 'July 4, 2020',
+    shipTo: 'JS Masher',
+    totalPrice: 17.22,
+    deliveryDate: 'July 7,2020',
+    products: [{
+      productName: 'Gamecube Controller-Adapter',
+      author: null,
+      returnWindow: 'Aug 5, 2020',
+      price: 15.98
+    }]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: 'July 3, 2020',
+    shipTo: 'JS Masher',
+    totalPrice: 138.93,
+    deliveryDate: 'July 5,2020',
+    products: [{
+      productName: 'Gamecube Controller - Super Smash bros. Edition',
+      author: null,
+      returnWindow: 'August 4, 2020',
+      price: 94.95
+    },
+    {
+      productName: 'The Art of Sql',
+      author: 'Faroult, Stephane',
+      returnWindow: 'August 4, 2020',
+      price: 33.99
+    }]
+  }
+];
+
+console.log('The value of orderHistory:', orderHistory);
