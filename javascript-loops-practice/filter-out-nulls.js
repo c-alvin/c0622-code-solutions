5d7c7b8acab9b3f746ea89271a1b0da4e0c1dded
Alvin Cho <alvinxcho2@gmail.com>
2022-07-08 22:04:51 +0000

    Completed javascript-loops-practice exercise.

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -1,4 +1,10 @@
 /* exported filterOutNulls */
+// goal = return a new array containing all of the origin input values xcept null
+// create new variable that is an empty array literal
+// iterate through each item in array
+// check if each item in the array is equal to null, if not enter into array.
+// return array
+
 function filterOutNulls(values) {
   var array = [];
   for (var i = 0; i < values.length; i++) {
