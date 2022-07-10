3104cf5e3f9ab1b34617287b91080ea6b5119511
Alvin Cho <alvinxcho2@gmail.com>
2022-07-10 05:34:13 +0000

    adjusted toObject.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -7,9 +7,6 @@
 
 function toObject(keyValuePair) {
   var obj = {};
-  for (var key in keyValuePair) {
-    key = keyValuePair[0];
-    obj[key] = keyValuePair[1];
-  }
+  obj[keyValuePair[0]] = keyValuePair[1];
   return obj;
 }
