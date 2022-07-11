f5f6d86ffeb3a379e89cc2922a50747d97183969
Alvin Cho <alvinxcho2@gmail.com>
2022-07-11 22:18:25 +0000

    array-manip-2 wip

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,17 @@
 /* exported chunk */
+// goal: return a new array of arrays, each sub-array has a length equal
+// to size.  remaining elements are gathered into a final sub-array
+// create an empty array
+// create a for loop to iterate through the array
+// if statement (count > array.length) newArray = array;
+// if statement
+
+/* function chunk(array, size) {
+ var chunkArray = [];
+ for (var i = 0; array.length; i++) {
+    if (size > array.length) {
+      chunkArray = array;
+    }
+  }
+}
+*/
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,20 @@
 /* exported takeRight */
+// goal: return an array consisting of the last count elements of the array
+// create an empty array
+// create a for loop, iterate through array at array.length - count;
+// create an if statement to check if count > array.length,
+// if so new array = array
+// else newarray.push(array[i])
+// return new array
+
+function takeRight(array, count) {
+  var rightArray = [];
+  for (var i = array.length - count; i < array.length; i++) {
+    if (count > array.length) {
+      rightArray = array;
+    } else {
+      rightArray.push(array[i]);
+    }
+  }
+  return rightArray;
+}
