bf3cc4492883567f4363fd3254ffad7fd1c1ac60
Alvin Cho <alvinxcho2@gmail.com>
2022-07-12 18:15:47 +0000

    array-manip-2 in progress.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,17 +1,25 @@
 /* exported chunk */
 // goal: return a new array of arrays, each sub-array has a length equal
 // to size.  remaining elements are gathered into a final sub-array
-// create an empty array
-// create a for loop to iterate through the array
-// if statement (count > array.length) newArray = array;
-// if statement
-
-// function chunk(array, size){
 //
-// var chunkArray = [];
-//  for (var i = 0; i < array.length; i += size) {
-//   if (size > array.length) {
-//    chunkArray = array;
-// } else if (size === 2) {
-//  chunkArray.push([array[i], array[i + 1]]);
-// } else if (size === 1) {
+
+function chunk(array, size) {
+  var counter = 0;
+
+  var chunkOfChunks = [];
+  var chunk = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (counter < size) {
+      chunk.push(array[i]);
+      counter++;
+    }
+
+    if (counter >= size || i === array.length - 1) {
+      chunkOfChunks.push(chunk);
+      chunk = [];
+      counter = 0;
+    }
+  }
+  return chunkOfChunks;
+}
