d2cf49a678c1e871d61cf5a9c2afc6430d76ec79
Alvin Cho <alvinxcho2@gmail.com>
2022-07-12 22:42:58 +0000

    changed variable name

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,15 @@
-var clicks = false;
+var lightOn = false;
 
 var $Button = document.querySelector('.button1');
 var $Background = document.querySelector('.background-black');
 
 function clickButton(event) {
-  if (clicks === false) {
-    clicks = true;
+  if (lightOn === false) {
+    lightOn = true;
     $Button.className = 'button2';
     $Background.className = 'background-yellow';
-  } else if (clicks === true) {
-    clicks = false;
+  } else if (lightOn === true) {
+    lightOn = false;
     $Button.className = 'button1';
     $Background.className = 'background-black';
   }
