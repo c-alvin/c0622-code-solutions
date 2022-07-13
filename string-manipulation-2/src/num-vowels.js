2204462b98b4c59c0d386d7ddeb1cfc29f71e93f
Alvin Cho <alvinxcho2@gmail.com>
2022-07-13 20:27:49 +0000

    adjusted numOfVowels

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -12,12 +12,10 @@ function numVowels(string) {
   var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
   var numOfVowels = 0;
   for (var i = 0; i < string.length; i++) {
-    var letter = string[i];
-    if (letter === vowels[0] || letter === vowels[1] || letter === vowels[2] ||
-      letter === vowels[3] || letter === vowels[4] || letter === vowels[5] ||
-      letter === vowels[6] || letter === vowels[7] || letter === vowels[8] ||
-      letter === vowels[9]) {
-      numOfVowels++;
+    for (var j = 0; j < vowels.length; j++) {
+      if (string[i] === vowels[j]) {
+        numOfVowels++;
+      }
     }
   }
   return numOfVowels;
