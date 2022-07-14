cab3b4c090aac97220d55c4678aa16c8a1d7c378
Alvin Cho <alvinxcho2@gmail.com>
2022-07-14 21:39:40 +0000

    wip

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -5,13 +5,13 @@
 // iterate through object by key, check if key is equal to ANY of
 // the keys, if so empty object.keyvariable ===
 
-function pick(source, keys) {
-  var object = {};
-  for (var x in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (x === keys[i]) {
-        object.x = x;
-      }
-    }
-  }
-}
+// function pick(source, keys) {
+//   var object = {};
+//   for (var x in source) {
+//     for (var i = 0; i < keys.length; i++) {
+//       if (x === keys[i]) {
+//         object = x;
+//       }
+//     }
+//   }
+// }
