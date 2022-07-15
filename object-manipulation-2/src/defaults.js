ae5888d49e3da6e6eddffc5eacb55fdf17477923
Alvin Cho <alvinxcho2@gmail.com>
2022-07-15 22:40:29 +0000

    wip

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,11 +1,12 @@
 /* exported defaults */
 // function defaults(target, source) {
+//   debugger;
 //   for (var key in source) {
 //     for (var prop in target) {
-//       if (target[key] === source[key]]) {
-//         target[prop] = source[key];
+//       if (key !== prop) {
+//         target[key] = source[key];
+//       }
 //       }
 //     }
-//   }
 //   return target;
 // }
