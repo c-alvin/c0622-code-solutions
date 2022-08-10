e275b6a98aa2095aeefc9a551a7a0c676cf1e73f
Alvin Cho <alvinxcho2@gmail.com>
2022-08-10 19:33:20 +0000

    Made a an array for suits and rank, created function to make deck

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -19,216 +19,25 @@ var players = [
   }
 ];
 
-var deckOfCards = [
-  {
-    rank: 'Ace',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Ace',
-    suit: 'spades'
-  },
-  {
-    rank: 'Ace',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Ace',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'King',
-    suit: 'clubs'
-  },
-  {
-    rank: 'King',
-    suit: 'spades'
-  },
-  {
-    rank: 'King',
-    suit: 'hearts'
-  },
-  {
-    rank: 'King',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Queen',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Queen',
-    suit: 'spades'
-  },
-  {
-    rank: 'Queen',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Queen',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Jack',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Jack',
-    suit: 'spades'
-  },
-  {
-    rank: 'Jack',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Jack',
-    suit: 'diamonds'
-  },
-  {
-    rank: '10',
-    suit: 'clubs'
-  },
-  {
-    rank: '10',
-    suit: 'spades'
-  },
-  {
-    rank: '10',
-    suit: 'hearts'
-  },
-  {
-    rank: '10',
-    suit: 'diamonds'
-  },
-  {
-    rank: '9',
-    suit: 'clubs'
-  },
-  {
-    rank: '9',
-    suit: 'spades'
-  },
-  {
-    rank: '9',
-    suit: 'hearts'
-  },
-  {
-    rank: '9',
-    suit: 'diamonds'
-  },
-  {
-    rank: '8',
-    suit: 'clubs'
-  },
-  {
-    rank: '8',
-    suit: 'spades'
-  },
-  {
-    rank: '8',
-    suit: 'hearts'
-  },
-  {
-    rank: '8',
-    suit: 'diamonds'
-  },
-  {
-    rank: '7',
-    suit: 'clubs'
-  },
-  {
-    rank: '7',
-    suit: 'spades'
-  },
-  {
-    rank: '7',
-    suit: 'hearts'
-  },
-  {
-    rank: '7',
-    suit: 'diamonds'
-  },
-  {
-    rank: '6',
-    suit: 'clubs'
-  },
-  {
-    rank: '6',
-    suit: 'spades'
-  },
-  {
-    rank: '6',
-    suit: 'hearts'
-  },
-  {
-    rank: '6',
-    suit: 'diamonds'
-  },
-  {
-    rank: '5',
-    suit: 'clubs'
-  },
-  {
-    rank: '5',
-    suit: 'spades'
-  },
-  {
-    rank: '5',
-    suit: 'hearts'
-  },
-  {
-    rank: '5',
-    suit: 'diamonds'
-  },
-  {
-    rank: '4',
-    suit: 'clubs'
-  },
-  {
-    rank: '4',
-    suit: 'spades'
-  },
-  {
-    rank: '4',
-    suit: 'hearts'
-  },
-  {
-    rank: '4',
-    suit: 'diamonds'
-  },
-  {
-    rank: '3',
-    suit: 'clubs'
-  },
-  {
-    rank: '3',
-    suit: 'spades'
-  },
-  {
-    rank: '3',
-    suit: 'hearts'
-  },
-  {
-    rank: '3',
-    suit: 'diamonds'
-  },
-  {
-    rank: '2',
-    suit: 'clubs'
-  },
-  {
-    rank: '2',
-    suit: 'spades'
-  },
-  {
-    rank: '2',
-    suit: 'hearts'
-  },
-  {
-    rank: '2',
-    suit: 'diamonds'
+var suit = ['clubs', 'spades', 'hearts', 'diamonds'];
+
+var rank = ['Ace', 'King', 'Queen', 'Jack', 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+var deckOfCards = [];
+
+function createDeck() {
+  for (var i = 0; i < suit.length; i++) {
+    for (var k = 0; k < rank.length; k++) {
+      var card = {};
+      card.suit = suit[i];
+      card.rank = rank[k];
+      deckOfCards.push(card);
+    }
   }
-];
+  return deckOfCards;
+}
+
+createDeck();
 
 var shuffledDeck = _.shuffle(deckOfCards);
 
@@ -261,12 +70,12 @@ function findWinner(players) {
     scoresArray[i] = 0;
     for (var j = 0; j < players[i].hand.length; j++) {
       if (players[i].hand[j].rank === 'Ace') {
-        scoresArray[i] += Number(11);
+        scoresArray[i] += 11;
       } else if (players[i].hand[j].rank === 'King' || players[i].hand[j].rank === 'Queen' || players[i].hand[j].rank === 'Jack') {
-        scoresArray[i] += Number(10);
+        scoresArray[i] += 10;
       } else {
         var value = players[i].hand[j].rank;
-        scoresArray[i] += Number(value);
+        scoresArray[i] += value;
       }
     }
   }
