780e730fd0a57a2636347299c0f206e59c08c44c
Alvin Cho <alvinxcho2@gmail.com>
2022-08-17 00:26:12 +0000

    Completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('the typeof:', typeof exports);
+console.log('the valueof:', exports);
+
+console.log('the typeof:', typeof require);
+console.log('the valueof:', require);
+
+console.log('the typeof:', typeof module);
+console.log('the valueof:', module);
+
+console.log('the typeof:', typeof __filename);
+console.log('the valueof:', __filename);
+
+console.log('the typeof:', typeof __dirname);
+console.log('the valueof:', __dirname);
