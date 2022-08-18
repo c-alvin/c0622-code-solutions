2cd4e192cd99fbeab1a13afa3dd47f94c535854f
Alvin Cho <alvinxcho2@gmail.com>
2022-08-18 18:50:56 +0000

    Completed node-notes-cli

---
+++ node-notes-cli/app.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const dataTest = require('./data.json');
+
+if (process.argv[2] === 'read') {
+  const notes = dataTest.notes;
+  for (const prop in notes) {
+    console.log(`${prop}: ${notes[prop]}`);
+  }
+}
+
+if (process.argv[2] === 'create') {
+  const notes = dataTest.notes;
+  notes[dataTest.nextId] = process.argv[3];
+  dataTest.nextId++;
+  const newData = JSON.stringify(dataTest, null, 2);
+  fs.writeFile('./data.json', newData, err => {
+    if (err) throw err;
+  });
+}
+
+if (process.argv[2] === 'delete') {
+  const notes = dataTest.notes;
+  for (const prop in notes) {
+    if (process.argv[3] === prop) {
+      delete notes[prop];
+    }
+  }
+  const newData = JSON.stringify(dataTest, null, 2);
+  fs.writeFile('./data.json', newData, err => {
+    if (err) throw err;
+  });
+}
+
+if (process.argv[2] === 'update') {
+  const notes = dataTest.notes;
+  for (const prop in notes) {
+    if (process.argv[3] === prop) {
+      notes[prop] = process.argv[4];
+    }
+  }
+  const newData = JSON.stringify(dataTest, null, 2);
+  fs.writeFile('./data.json', newData, err => {
+    if (err) throw err;
+  });
+}
