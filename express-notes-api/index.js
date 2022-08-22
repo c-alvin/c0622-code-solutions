50e1fac362fb0b93f4b6f4edc2c61db8b55680be
Alvin Cho <alvinxcho2@gmail.com>
2022-08-22 22:57:43 +0000

    Completed express-notes-api

---
+++ express-notes-api/index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+
+const app = express();
+
+const data = require('./data.json');
+
+const fs = require('fs');
+
+app.get('/api/notes', (req, res) => {
+  const arrayObjects = [];
+  const notes = data.notes;
+  for (const key in notes) {
+    arrayObjects.push(notes[key]);
+  }
+  res.send(arrayObjects);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const notes = data.notes;
+  const number = Number(req.params.id);
+  if (number < 0) {
+    res.status(400).send({
+      error: 'id must be a postiive integer'
+    });
+  }
+  if (notes[number] === undefined) {
+    res.status(404).json({
+      error: 'no note with the specified id'
+    });
+  }
+  if (notes[number] !== null) {
+    res.status(200).json(notes[number]);
+  }
+});
+
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+  const postObject = req.body.content;
+  const notes = data.notes;
+  const Object = req.body;
+  if (!postObject) {
+    res.status(400).json({
+      error: 'content is a required field'
+    });
+  }
+  if (postObject) {
+    Object.Id = data.nextId;
+    notes[data.nextId] = Object;
+    data.nextId++;
+    const newData = JSON.stringify(data, null, 2);
+    fs.writeFile('./data.json', newData, err => {
+      JSON.error = 'An unexpected error occurred.';
+      if (!err) {
+        res.status(201).send(Object);
+      } else {
+        res.status(500).send((JSON.error));
+      }
+    });
+  }
+}
+);
+
+app.delete('/api/notes/:id', (req, res) => {
+  const number = Number(req.params.id);
+  const notes = data.notes;
+  if (number < 0) {
+    res.status(400).send({
+      error: 'id must be a postiive integer'
+    });
+  }
+  if (notes[number] === undefined) {
+    res.status(404).json({
+      error: 'no note with the specified id'
+    });
+  }
+  if (notes[number] !== null) {
+    delete notes[number];
+    const newData = JSON.stringify(data, null, 2);
+    fs.writeFile('./data.json', newData, err => {
+      JSON.error = 'An unexpected error occurred.';
+      if (!err) {
+        res.sendStatus(204);
+      } else {
+        res.status(500).send((JSON.error));
+      }
+    });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const number = Number(req.params.id);
+  const notes = data.notes;
+  const postObject = req.body.content;
+  if (number < 0) {
+    res.status(400).send({
+      error: 'id must be a postiive integer'
+    });
+  }
+  if (!postObject) {
+    res.status(400).send({
+      error: 'content is a required field'
+    });
+  }
+  if (notes[number] === undefined) {
+    res.status(404).json({
+      error: 'no note with the specified id'
+    });
+  }
+  if (notes[number] !== null && postObject) {
+    notes[number].content = postObject;
+    const newData = JSON.stringify(data, null, 2);
+    fs.writeFile('./data.json', newData, err => {
+      JSON.error = 'An unexpected error occurred.';
+      if (!err) {
+        res.status(200).send(notes[number]);
+      } else {
+        res.status(500).send((JSON.error));
+      }
+    });
+  }
+});
