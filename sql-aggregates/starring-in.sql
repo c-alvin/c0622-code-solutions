48a59c424e62c383049de0660691b2c371e5709a
Alvin Cho <alvinxcho2@gmail.com>
2022-08-24 23:53:27 +0000

    Changed where conditon

--- sql-aggregates/starring-in.sql
+++ sql-aggregates/starring-in.sql
@@ -4,5 +4,6 @@ select "g"."name" as "genreName",
    join "castMembers" using ("actorId")
    join "filmGenre" using ("filmId")
    join "genres" as "g" using ("genreId")
-   where "actors"."actorId" = 178
+   where "actors"."firstName" = 'Lisa' AND
+        "actors"."lastName" = 'Monroe'
    group by "g"."genreId";
