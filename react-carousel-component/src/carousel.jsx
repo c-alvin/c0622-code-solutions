e7b5c569ce5599528521e6bc0e1bfe28c37f844c
Alvin Cho <alvinxcho2@gmail.com>
2022-09-02 22:09:51 +0000

    wip

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,8 +1,31 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
 
-// export default class Carousel extends React.Component {
-//   render() {
-//     <img src=""alt="" />;
-//   }
-// }
+export default class Carousel extends React.Component {
+  render() {
+    return (
+    <div className='container'>
+      <div className='box'>
+        <div className='row'>
+          <div className='column'> </div>
+            <i className="fa-solid fa-chevron-left"></i>
+          <div className='column'> </div>
+            <img src="" alt="" />
+          <div className='column'> </div>
+            <i className="fa-solid fa-chevron-right"></i>
+        </div>
+        <div className="row">
+          <div className='column'>
+            <div className='row'>
+              <i className="fa-regular fa-circle" ></i>
+              <i className="fa-regular fa-circle" ></i>
+              <i className="fa-regular fa-circle" ></i>
+              <i className="fa-regular fa-circle" ></i>
+              <i className="fa-regular fa-circle" ></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
 
 const images = [
-  'https://static.wikia.nocookie.net/terminalmontage/images/c/c0/Kirbo.png/revision/latest?cb=20200609234320',
-  'https://static.wikia.nocookie.net/terminalmontage/images/7/74/Melee_Fox_TM.png/revision/latest/top-crop/width/360/height/360?cb=20200617045035',
-  'https://static.wikia.nocookie.net/terminalmontage/images/e/ec/Luigi_TM_Edit_Cut.png/revision/latest/top-crop/width/360/height/360?cb=20200724034944'
+  {
+    link: 'https://static.wikia.nocookie.net/terminalmontage/images/c/c0/Kirbo.png/revision/latest?cb=20200609234320'
+  },
+  {
+    link: 'https://static.wikia.nocookie.net/terminalmontage/images/7/74/Melee_Fox_TM.png/revision/latest/top-crop/width/360/height/360?cb=20200617045035'
+  },
+  {
+    link: 'https://static.wikia.nocookie.net/terminalmontage/images/e/ec/Luigi_TM_Edit_Cut.png/revision/latest/top-crop/width/360/height/360?cb=20200724034944'
+  }
 ];
 const container = document.querySelector('#root');
 
