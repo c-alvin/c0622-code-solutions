271d6cc3f5eff38844108d16164973f8254ca287
Alvin Cho <alvinxcho2@gmail.com>
2022-09-02 18:11:13 +0000

    wip carousel

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// export default class Carousel extends React.Component {
+//   render() {
+//     <img src=""alt="" />;
+//   }
+// }
