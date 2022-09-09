053c30397ee04146bc9a96d5e684765aa14d7ecc
Alvin Cho <alvinxcho2@gmail.com>
2022-09-09 17:46:51 +0000

    got carousel to click, and move on interval

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -6,37 +6,81 @@ export default class Carousel extends React.Component {
     this.state = {
       currentState: 0
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickLeft = this.handleClickLeft.bind(this);
+  }
+
+  handleClick(event) {
+    if (this.state.currentState <= 3) {
+      this.setState(prevState => ({
+        currentState: prevState.currentState + 1
+      }));
+    } else {
+      this.setState(prevState => ({
+        currentState: 0
+      }));
+    }
+  }
+
+  handleClickLeft(event) {
+    if (this.state.currentState >= 1) {
+      this.setState(prevState => ({
+        currentState: prevState.currentState - 1
+      }));
+    } else {
+      this.setState(prevState => ({
+        currentState: 4
+      }));
+    }
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      if (this.state.currentState <= 3) {
+        this.setState(prevState => ({
+          currentState: prevState.currentState + 1
+        }));
+      } else {
+        this.setState({
+          currentState: 0
+        });
+      }
+    }, 3000);
   }
 
   render() {
-    const listImage =
-    this.props.images.map((topic, index) => {
+    const circles =
+    this.props.images.map((circle, index) => {
+      const circlecolor = this.state.currentState === index
+        ? 'fa-solid'
+        : 'fa-regular';
       return (
-          <img data-images={index} key={index} src={topic.link}></img>
+          <i key={index} className={`${circlecolor} fa-circle circle`} ></i>
+      );
+    });
+    const images = this.props.images.map((image, index) => {
+      return (
+        <img key={index} src={image.link}></img>
       );
     });
     return (
     <div className='container'>
       <div className='box'>
         <div className='row'>
-            <div className='column-fifth display-flex align-center justify-center'>
-              <i className="fa-solid fa-chevron-left chevron left"></i>
+            <div className='column-fifth display-flex align-center justify-center' onClick={this.handleClickLeft}>
+              <i className="fa-solid  fa-chevron-left chevron left"></i>
             </div>
             <div className='column-three-fifth display-flex align-center justify-center'>
-              {listImage}
+              {images[this.state.currentState]}
             </div>
-            <div className='column-fifth display-flex align-center justify-center'>
+            <div className='column-fifth display-flex align-center justify-center' onClick={this.handleClick}>
               <i className="fa-solid fa-chevron-right chevron right"></i>
             </div>
         </div>
         <div className="row">
             <div className='column-half margin-auto'>
               <div className='row justify-center space-evenly margin-bot-small circle-row padding-small'>
-                <i className="fa-regular fa-circle circle " ></i>
-                <i className="fa-regular fa-circle circle" ></i>
-                <i className="fa-regular fa-circle circle" ></i>
-                <i className="fa-regular fa-circle circle" ></i>
-                <i className="fa-regular fa-circle circle" ></i>
+                {circles}
             </div>
           </div>
         </div>
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -10,7 +10,13 @@ const images = [
     link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB66mMfXcl7zUvfgf1JfWFL7IyQwRyxJHzfbjPyEAU501hwmhfmTb13eSYdfatyAr5qXs&usqp=CAU'
   },
   {
-    link: 'https://www.vhv.rs/dpng/d/163-1631002_terminalmontage-luigi-hd-png-download.png'
+    link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCPl9-5JRmY144cE2vUrlo8aONCMWlVtgalBXvd3Aowvi43Ga_s1B7FUQpJtZg6wr1k4&usqp=CAU'
+  },
+  {
+    link: 'https://i.redd.it/hllby89s8f451.png'
+  },
+  {
+    link: 'https://static.wikia.nocookie.net/1efa259a-bfd6-442e-b5fa-23b0b1db264b'
   }
 ];
 const container = document.querySelector('#root');
